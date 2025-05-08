5d9bd872707051a60de817f06c469a4a66375a3e
Rodrigo Vieira <rodrigo.vieira.t0126458@edu.atec.pt>
2025-05-08 15:01:33 +0100

    Set the type of buffers

--- src/opengl/opengl-buffer.cpp
+++ src/opengl/opengl-buffer.cpp
@@ -4,6 +4,7 @@
 namespace alina::opengl {
     Buffer::Buffer(const BufferDesc& desc, Device* device) {
         mDevice = static_cast<Device*>(device);
+        mType = desc.type;
         auto e = bufferTypeToGl(desc.type);
         mDevice->context.CreateBuffers(1, &mID);
         mDevice->context.ObjectLabel(GL_BUFFER, mID, desc.name.size(), desc.name.data());
