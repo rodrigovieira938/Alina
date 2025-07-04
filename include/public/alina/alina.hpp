d8ca93de073146f0b1aceb9b10b85a4aa3a5a355
Rodrigo Vieira <rodrigo.vieira.t0126458@edu.atec.pt>
2025-07-04 14:15:53 +0100

    Organizing alina.hpp

--- include/public/alina/alina.hpp
+++ include/public/alina/alina.hpp
@@ -5,26 +5,24 @@
 #include <vector>
 
 namespace alina {
+    // ===================
+    // Handles
+    // ===================
+    class IBuffer;
+    class IInputLayout;
+    class IShader;
+    class ISampler;
+    class ITexture;
+    class IFramebuffer;
+    class ICommandList;
+    class IDevice;
+    // ===================
+    // Enums
+    // ===================
     enum class BufferType {
         VERTEX,
         INDEX,
         UNIFORM
-    };
-    struct BufferDesc {
-        BufferDesc& setDebugName(std::string name) {
-            this->name = name;
-            return *this;
-        };
-        BufferDesc& setType(BufferType type) {
-            this->type = type;
-            return *this;
-        };
-        std::string name = "";
-        BufferType type = BufferType::VERTEX;
-    };
-    class IBuffer {
-    public:
-        virtual BufferType getType() const = 0; 
     };
     enum class PrimitiveType : uint8_t
     {
@@ -47,102 +45,21 @@ namespace alina {
         Fill = Solid,
         Line = Wireframe
     };
-
     enum class RasterCullMode : uint8_t
     {
         Back,
         Front,
         None
     };
-
-    struct RasterState
-    {
-        RasterFillMode fillMode = RasterFillMode::Solid;
-        RasterCullMode cullMode = RasterCullMode::Back;
-        bool frontCounterClockwise = false;
-        RasterState& setFillMode(RasterFillMode value) { fillMode = value; return *this; }
-        RasterState& setFillSolid() { fillMode = RasterFillMode::Solid; return *this; }
-        RasterState& setFillWireframe() { fillMode = RasterFillMode::Wireframe; return *this; }
-        RasterState& setCullMode(RasterCullMode value) { cullMode = value; return *this; }
-        RasterState& setCullBack() { cullMode = RasterCullMode::Back; return *this; }
-        RasterState& setCullFront() { cullMode = RasterCullMode::Front; return *this; }
-        RasterState& setCullNone() { cullMode = RasterCullMode::None; return *this; }
-        RasterState& setFrontCounterClockwise(bool value) { frontCounterClockwise = value; return *this; }
-    };
-    struct RenderState {
-        RasterState rasterState;
-
-        RenderState& setRasterState(const RasterState& value) { rasterState = value; return *this; }
-    };
     enum class VertexAttributeFormat {
         Int,
         Unsigned_Int,
         Float,
     };
-    struct VertexAttributeDesc {
-        std::string name;
-        uint32_t bufferIndex = 0;
-        uint32_t arraySize = 1;
-        uint32_t offset = 0;
-        uint32_t stride = 0;
-        bool isInstanced = false;
-        VertexAttributeFormat format;
-
-        VertexAttributeDesc& setName(std::string value) {name = value; return *this;}
-        VertexAttributeDesc& setBufferIndex(uint32_t value) {bufferIndex = value; return *this;}
-        VertexAttributeDesc& setArraySize(uint32_t value) {arraySize = value; return *this;}
-        VertexAttributeDesc& setOffset(uint32_t value) {offset = value; return *this;}
-        VertexAttributeDesc& setStride(uint32_t value) {stride = value; return *this;}
-        VertexAttributeDesc& setFormat(VertexAttributeFormat value) {format = value; return *this;}
-        VertexAttributeDesc& setInstanced(bool value) {isInstanced = value; return *this;}
-
-    };
-    class IInputLayout {
-    public:
-        virtual VertexAttributeDesc* getAttributes() = 0;
-        virtual size_t getNumAttributes() = 0;
-    };
     enum class ShaderType {
         //TODO: add more
         VERTEX,
         FRAGMENT
-    };
-    class IShader {
-    public:
-        virtual ShaderType getType() = 0;
-    };
-    //Simple graphics pipeline desc, must be expanded
-    struct GraphicsPipelineDesc {
-        PrimitiveType primType = PrimitiveType::TriangleList;
-        RenderState renderState;
-        IInputLayout* inputLayout = nullptr;
-        IShader *vs = nullptr,*fs = nullptr;
-
-        GraphicsPipelineDesc& setPrimType(PrimitiveType type) { primType = type; return *this; }
-        GraphicsPipelineDesc& setRenderState(RenderState state) { renderState = state; return *this; }
-        GraphicsPipelineDesc& setInputLayout(IInputLayout* value) { inputLayout = value; return *this; }
-        GraphicsPipelineDesc& setVertexShader(IShader* value) { vs = value; return *this; }
-        GraphicsPipelineDesc& setFragmentShader(IShader* value) { fs = value; return *this; }
-    };
-    class IGraphicsPipeline {
-    };
-    struct DrawArguments
-    {
-        uint32_t vertexCount = 0;
-        uint32_t offset = 0;
-        uint32_t instanceCount = 1;
-
-        DrawArguments& setVertexCount(uint32_t value) { vertexCount = value; return *this; }
-        DrawArguments& setOffset(uint32_t value) { offset = value; return *this; }
-        DrawArguments& setInstanceCount(uint32_t value) { instanceCount = value; return *this; }
-    };
-    struct BindVertexBuffer {
-        IBuffer* buffer = 0;
-        uint32_t offset = 0, stride = 0;
-
-        BindVertexBuffer& setBuffer(IBuffer*  value) {buffer = value; return *this;}
-        BindVertexBuffer& setOffset(uint32_t value) {offset = value; return *this;}
-        BindVertexBuffer& setStride(uint32_t value) {stride = value; return *this;}
     };
     enum class TextureFormat {
         #define FORMAT(type, bits) \
@@ -183,79 +100,65 @@ namespace alina {
         ClampToEdge,
         ClampToBorder,
     };
-    struct Color {
-        float r=.0f,g=.0f,b=.0f,a = 1.0;
-    };
-    struct SamplerDesc {
-        TextureFilter filter = TextureFilter::Bilinear;
-        bool minFilter = true, magFilter = true, mipFilter = true;
-        TextureWrap u = TextureWrap::ClampToEdge,v = TextureWrap::ClampToEdge,w = TextureWrap::ClampToEdge;
-        Color border = {1,1,1,1};
-
-        SamplerDesc& setFilter(TextureFilter value) {filter = value; return *this;}
-        SamplerDesc& setAllFilters(bool value) {minFilter = magFilter = mipFilter = value; return *this;}
-        SamplerDesc& setMinFilter(bool value) {minFilter = value; return *this;}
-        SamplerDesc& setMagFilter(bool value) {magFilter = value; return *this;}
-        SamplerDesc& setMipFilter(bool value) {mipFilter = value; return *this;}
-        SamplerDesc& setAllWraps(TextureWrap value) {u = v = w = value; return *this;}
-        SamplerDesc& setUWrap(TextureWrap value) {u = value; return *this;}
-        SamplerDesc& setVWrap(TextureWrap value) {v = value; return *this;}
-        SamplerDesc& setWWrap(TextureWrap value) {w = value; return *this;}
-        SamplerDesc& setAllWrap(TextureWrap value) {u = v = w = value; return *this;}
-    };
     enum class TextureUsage {
         TEXTURE,
         ATTACHMENT,
     };
-    //TODO: add clearColor
-    struct TextureDesc {
-        TextureFormat format = TextureFormat::RGB8_Unorm;
-        uint32_t width = 1, height = 1;
-        uint32_t depth = 1;
-        // 0 -> none
-        uint32_t mipLevels = 0;
-        SamplerDesc sampler;
-        TextureUsage usage = TextureUsage::TEXTURE;
-        std::string name; 
-
-        TextureDesc& setFormat(TextureFormat value) {format = value; return *this;}
-        TextureDesc& setWidth(uint32_t value) {width = value; return *this;}
-        TextureDesc& setHeight(uint32_t value) {height = value; return *this;}
-        TextureDesc& setDepth(uint32_t value) {depth = value; return *this;}
-        TextureDesc& setMipLevels(uint32_t value) {mipLevels = value; return *this;}
-        TextureDesc& setSampler(const SamplerDesc& value) {sampler = value; return *this;}
-        TextureDesc& setUsage(TextureUsage value) {usage = value; return *this;}
-        TextureDesc& setDebugName(std::string name) {
-            this->name = name;
-            return *this;
-        };
+    enum class RenderPassLoadOp {
+        LOAD, CLEAR
     };
-    class ISampler {
-
+    enum class SubPassAttachment {
+        NONE,
+        PRESERVE,
+        INPUT,
+        COLOR,
     };
-    class ITexture {
-    public:
-        virtual TextureFormat getFormat() = 0;
+    // ===================
+    // Structs
+    // ===================
+    struct RasterState
+    {
+        RasterFillMode fillMode = RasterFillMode::Solid;
+        RasterCullMode cullMode = RasterCullMode::Back;
+        bool frontCounterClockwise = false;
+        RasterState& setFillMode(RasterFillMode value) { fillMode = value; return *this; }
+        RasterState& setFillSolid() { fillMode = RasterFillMode::Solid; return *this; }
+        RasterState& setFillWireframe() { fillMode = RasterFillMode::Wireframe; return *this; }
+        RasterState& setCullMode(RasterCullMode value) { cullMode = value; return *this; }
+        RasterState& setCullBack() { cullMode = RasterCullMode::Back; return *this; }
+        RasterState& setCullFront() { cullMode = RasterCullMode::Front; return *this; }
+        RasterState& setCullNone() { cullMode = RasterCullMode::None; return *this; }
+        RasterState& setFrontCounterClockwise(bool value) { frontCounterClockwise = value; return *this; }
+    };
+    struct RenderState {
+        RasterState rasterState;
+
+        RenderState& setRasterState(const RasterState& value) { rasterState = value; return *this; }
+    };
+    struct DrawArguments
+    {
+        uint32_t vertexCount = 0;
+        uint32_t offset = 0;
+        uint32_t instanceCount = 1;
+
+        DrawArguments& setVertexCount(uint32_t value) { vertexCount = value; return *this; }
+        DrawArguments& setOffset(uint32_t value) { offset = value; return *this; }
+        DrawArguments& setInstanceCount(uint32_t value) { instanceCount = value; return *this; }
+    };
+    struct BindVertexBuffer {
+        IBuffer* buffer = 0;
+        uint32_t offset = 0, stride = 0;
+
+        BindVertexBuffer& setBuffer(IBuffer*  value) {buffer = value; return *this;}
+        BindVertexBuffer& setOffset(uint32_t value) {offset = value; return *this;}
+        BindVertexBuffer& setStride(uint32_t value) {stride = value; return *this;}
+    };
+    struct Color {
+        float r=.0f,g=.0f,b=.0f,a = 1.0;
     };
     struct FramebufferAttachment {
         ITexture* texture;
         FramebufferAttachment& setTexture(ITexture* value) {texture = value; return *this;}
-    };
-    //TODO: add acess to only a specific layer
-    struct FramebufferDesc {
-        std::array<FramebufferAttachment, 8> colorAttachments;
-        FramebufferAttachment depthAttachment;
-        std::string name;
-
-        FramebufferDesc& setColorAttachments(const std::array<FramebufferAttachment, 8>& value) {colorAttachments = value; return *this;}
-        FramebufferDesc& setDepthAttachment(FramebufferAttachment value) {depthAttachment = value; return *this;}
-        FramebufferDesc& setDebugName(std::string name) {
-            this->name = name;
-            return *this;
-        };
-    };
-    class IFramebuffer {
-
     };
     struct UniformBufferBinding {
         IBuffer* buffer = nullptr;
@@ -282,8 +185,87 @@ namespace alina {
         ShaderResources& setUboBindings(const std::vector<UniformBufferBinding> value) {uboBinding = value; return *this;}
         ShaderResources& setTexBindings(const std::vector<TextureBinding> value)       {texBinding = value; return *this;}
     };
-    enum class RenderPassLoadOp {
-        LOAD, CLEAR
+    // ===================
+    // Interface Descs
+    // ===================
+
+    struct BufferDesc {
+        BufferDesc& setDebugName(std::string name) {
+            this->name = name;
+            return *this;
+        };
+        BufferDesc& setType(BufferType type) {
+            this->type = type;
+            return *this;
+        };
+        std::string name = "";
+        BufferType type = BufferType::VERTEX;
+    };
+    //Simple graphics pipeline desc, must be expanded
+    struct GraphicsPipelineDesc {
+        PrimitiveType primType = PrimitiveType::TriangleList;
+        RenderState renderState;
+        IInputLayout* inputLayout = nullptr;
+        IShader *vs = nullptr,*fs = nullptr;
+
+        GraphicsPipelineDesc& setPrimType(PrimitiveType type) { primType = type; return *this; }
+        GraphicsPipelineDesc& setRenderState(RenderState state) { renderState = state; return *this; }
+        GraphicsPipelineDesc& setInputLayout(IInputLayout* value) { inputLayout = value; return *this; }
+        GraphicsPipelineDesc& setVertexShader(IShader* value) { vs = value; return *this; }
+        GraphicsPipelineDesc& setFragmentShader(IShader* value) { fs = value; return *this; }
+    };
+    struct SamplerDesc {
+        TextureFilter filter = TextureFilter::Bilinear;
+        bool minFilter = true, magFilter = true, mipFilter = true;
+        TextureWrap u = TextureWrap::ClampToEdge,v = TextureWrap::ClampToEdge,w = TextureWrap::ClampToEdge;
+        Color border = {1,1,1,1};
+
+        SamplerDesc& setFilter(TextureFilter value) {filter = value; return *this;}
+        SamplerDesc& setAllFilters(bool value) {minFilter = magFilter = mipFilter = value; return *this;}
+        SamplerDesc& setMinFilter(bool value) {minFilter = value; return *this;}
+        SamplerDesc& setMagFilter(bool value) {magFilter = value; return *this;}
+        SamplerDesc& setMipFilter(bool value) {mipFilter = value; return *this;}
+        SamplerDesc& setAllWraps(TextureWrap value) {u = v = w = value; return *this;}
+        SamplerDesc& setUWrap(TextureWrap value) {u = value; return *this;}
+        SamplerDesc& setVWrap(TextureWrap value) {v = value; return *this;}
+        SamplerDesc& setWWrap(TextureWrap value) {w = value; return *this;}
+        SamplerDesc& setAllWrap(TextureWrap value) {u = v = w = value; return *this;}
+    };
+    //TODO: add clearColor
+    struct TextureDesc {
+        TextureFormat format = TextureFormat::RGB8_Unorm;
+        uint32_t width = 1, height = 1;
+        uint32_t depth = 1;
+        // 0 -> none
+        uint32_t mipLevels = 0;
+        SamplerDesc sampler;
+        TextureUsage usage = TextureUsage::TEXTURE;
+        std::string name; 
+
+        TextureDesc& setFormat(TextureFormat value) {format = value; return *this;}
+        TextureDesc& setWidth(uint32_t value) {width = value; return *this;}
+        TextureDesc& setHeight(uint32_t value) {height = value; return *this;}
+        TextureDesc& setDepth(uint32_t value) {depth = value; return *this;}
+        TextureDesc& setMipLevels(uint32_t value) {mipLevels = value; return *this;}
+        TextureDesc& setSampler(const SamplerDesc& value) {sampler = value; return *this;}
+        TextureDesc& setUsage(TextureUsage value) {usage = value; return *this;}
+        TextureDesc& setDebugName(std::string name) {
+            this->name = name;
+            return *this;
+        };
+    };
+    //TODO: add acess to only a specific layer
+    struct FramebufferDesc {
+        std::array<FramebufferAttachment, 8> colorAttachments;
+        FramebufferAttachment depthAttachment;
+        std::string name;
+
+        FramebufferDesc& setColorAttachments(const std::array<FramebufferAttachment, 8>& value) {colorAttachments = value; return *this;}
+        FramebufferDesc& setDepthAttachment(FramebufferAttachment value) {depthAttachment = value; return *this;}
+        FramebufferDesc& setDebugName(std::string name) {
+            this->name = name;
+            return *this;
+        };
     };
     struct RenderPassDesc {
         IFramebuffer* fb = nullptr;
@@ -294,16 +276,58 @@ namespace alina {
         RenderPassDesc& setAttachmentsClearColors(const std::array<std::array<float, 4>, 8>& value) {clearColors = value; return *this;}
     
     };
-    enum class SubPassAttachment {
-        NONE,
-        PRESERVE,
-        INPUT,
-        COLOR,
-    };
     struct SubPassDesc {
         std::array<SubPassAttachment, 8> attachments;
 
         SubPassDesc& setAttachments(const std::array<SubPassAttachment, 8>& value) {attachments = value; return *this;}
+    };
+    struct VertexAttributeDesc {
+        std::string name;
+        uint32_t bufferIndex = 0;
+        uint32_t arraySize = 1;
+        uint32_t offset = 0;
+        uint32_t stride = 0;
+        bool isInstanced = false;
+        VertexAttributeFormat format;
+
+        VertexAttributeDesc& setName(std::string value) {name = value; return *this;}
+        VertexAttributeDesc& setBufferIndex(uint32_t value) {bufferIndex = value; return *this;}
+        VertexAttributeDesc& setArraySize(uint32_t value) {arraySize = value; return *this;}
+        VertexAttributeDesc& setOffset(uint32_t value) {offset = value; return *this;}
+        VertexAttributeDesc& setStride(uint32_t value) {stride = value; return *this;}
+        VertexAttributeDesc& setFormat(VertexAttributeFormat value) {format = value; return *this;}
+        VertexAttributeDesc& setInstanced(bool value) {isInstanced = value; return *this;}
+
+    };
+    // ===================
+    // Interfaces
+    // ===================
+
+    class IBuffer {
+    public:
+        virtual BufferType getType() const = 0; 
+    };
+
+    class IInputLayout {
+    public:
+        virtual VertexAttributeDesc* getAttributes() = 0;
+        virtual size_t getNumAttributes() = 0;
+    };
+    class IShader {
+    public:
+        virtual ShaderType getType() = 0;
+    };
+    class IGraphicsPipeline {
+    };
+    class ISampler {
+
+    };
+    class ITexture {
+    public:
+        virtual TextureFormat getFormat() = 0;
+    };
+    class IFramebuffer {
+
     };
     class ICommandList {
     public:
